da5f566e0098c77f5c7ce948a9b69a158fb0b425
M1053737 <54942737+M1053737@users.noreply.github.com>
2020-09-02 13:31:52 +0530

    Update pushToGitlabRepository.groovy

--- vars/pushToGitlabRepository.groovy
+++ vars/pushToGitlabRepository.groovy
@@ -7,7 +7,8 @@ def call() {
     pipelinePlaceholders = PipelinePlaceholders.getInstance()
 
     sh "git init"
-    sh "git remote add origin ${pipelinePlaceholders.getSshUrlToRepo()}"
+    sh "git remote add origin gitlab
+    // ${pipelinePlaceholders.getSshUrlToRepo()}"
     push("Initial commit", "master")
 
     // ---------------------------- NEEDS TO BE CHANGED TO SUPPORT BRANCHING ----------------------------
