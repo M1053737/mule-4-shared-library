50b1d1dd129909a90c31cec08005d96e6fb6d54f
M1053737 <54942737+M1053737@users.noreply.github.com>
2020-09-02 12:56:50 +0530

    Update createGitRepository.groovy

--- vars/createGitRepository.groovy
+++ vars/createGitRepository.groovy
@@ -31,7 +31,7 @@ def call() {
             customHeaders: [[name: 'Authorization', value: "Token 47c673a32b2297050d5a93563b6c2663ca0fdeb9"], [name: 'Content-Type', value: 'application/json']],
             quiet: true,
             requestBody: repo_create_req,
-            validResponseCodes: '200:404:401'
+            validResponseCodes: '200:404'
         )
 
     def responseMap = new groovy.json.JsonSlurperClassic().parseText(response.content)
