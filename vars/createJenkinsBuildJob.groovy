1d51ab5bc0dee6cdcf4d09dcfd20dc220789dc35
M1053737 <54942737+M1053737@users.noreply.github.com>
2020-09-04 20:39:25 +0530

    Update createJenkinsBuildJob.groovy

--- vars/createJenkinsBuildJob.groovy
+++ vars/createJenkinsBuildJob.groovy
@@ -32,14 +32,9 @@ def call() {
 
     def payload = readFile "buildConfig.xml"
     echo payload
-    def url =  "http://${Constants.JENKINS_DOMAIN}/createItem?name=${jobName}"
-    echo url
-    
-    def urlget= "http://52.172.43.67:8080/crumbIssuer/api/json"
-    echo urlget
-    
     
     echo "******************KRISHNA START *************"
+     
      def crumbResponse = httpRequest 'http://52.172.43.67:8080/crumbIssuer/api/json'
          println("Status: "+crumbResponse.status)
          println("Content: "+crumbResponse.content)
@@ -52,19 +47,16 @@ def call() {
    
     def response = httpRequest (
         httpMode: "POST",
-        url:url,
-       //url: "http://${Constants.JENKINS_DOMAIN}/createItem?name=${jobName}",
-      //  url: "http://52.172.43.67:8080/createItem?name=NEWJOB4534222321",
+        url: "http://${Constants.JENKINS_DOMAIN}/createItem?name=${jobName}",
+        //url: "http://52.172.43.67:8080/createItem?name=NEWJOB4534222321",
         //url: "http://${Constants.JENKINS_DOMAIN}/job/${folderName}/createItem?name=${jobName}",
-        
         //customHeaders: [[name: 'Authorization', value: "Token ${authString}"], [name: 'Content-Type', value: 'application/xml']],
+        requestBody: payload,
         customHeaders: [[name: 'Authorization', value: "Basic YWRtaW46YWRtaW4xMjM="], [name: 'Content-Type', value: 'application/xml'], [name: 'Jenkins-Crumb', value: "${crumbCode}"]],
         //customHeaders: [[name: 'Authorization', value: "Token ${authString}"], [name: 'Content-Type', value: 'application/xml'], [name: 'Jenkins-Crumb', value: "55f092c2df3ecf5d682d8e6d74b2f8c2faea389f9c143755eebba4d61ee19552"]],
-        quiet: true,
-        requestBody: payload
+        quiet: true
         //validResponseCodes: '200:408'
-   
-    )
+     )
     
     sh "rm buildConfig.xml"
 }
