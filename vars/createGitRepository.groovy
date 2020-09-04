304cad5388cba42ffc0a43ff398da2c0670bc261
M1053737 <54942737+M1053737@users.noreply.github.com>
2020-09-04 23:59:28 +0530

    Update createGitRepository.groovy

--- vars/createGitRepository.groovy
+++ vars/createGitRepository.groovy
@@ -30,7 +30,7 @@ def call() {
             url: url,
          //   customHeaders: [[name: 'Authorization', value: "Basic bmFnZW5kcmEuY2EyQG1pbmR0cmVlLmNvbTpxenBtQDE5OTc="], [name: 'Content-Type', value: 'application/json']],
          customHeaders: [[name: 'Authorization', value: "Token 2e81f817f554e1a4565fc5cac0c8a5f3d4a28346"], [name: 'Content-Type', value: 'application/json']],
-           // quiet: true,
+           quiet: true,
             requestBody: repo_create_req
             //validResponseCodes: '200:401'
         )
