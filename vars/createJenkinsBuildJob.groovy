aba6975edc34b0ab35d009b40d2e7e20f023b523
M1053737 <54942737+M1053737@users.noreply.github.com>
2020-09-05 21:05:45 +0530

    Update createJenkinsBuildJob.groovy

--- vars/createJenkinsBuildJob.groovy
+++ vars/createJenkinsBuildJob.groovy
@@ -3,6 +3,18 @@ import com.mulesoft.PipelinePlaceholders
 import com.mulesoft.Secrets
 import groovy.json.JsonSlurperClassic
 
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.net.URLConnection;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.cert.X509Certificate;
+
 
 
 def call() {
@@ -47,6 +59,37 @@ def call() {
     
     def payload = readFile "buildConfig.xml"
     echo payload
+
+        echo "***********START***********"
+
+    // Create a trust manager that does not validate certificate chains
+TrustManager[] trustAllCerts = [ new X509TrustManager() {
+        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+            return null;
+        }
+        public void checkClientTrusted(X509Certificate[] certs, String authType) {
+        }
+        public void checkServerTrusted(X509Certificate[] certs, String authType) {
+        }
+    }
+];
+
+// Install the all-trusting trust manager
+SSLContext sc = SSLContext.getInstance("SSL");
+sc.init(null, trustAllCerts, new java.security.SecureRandom());
+HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+// Create all-trusting host name verifier
+HostnameVerifier allHostsValid = new HostnameVerifier() {
+    public boolean verify(String hostname, SSLSession session) {
+        return true;
+    }
+};
+
+// Install the all-trusting host verifier
+HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+    
+    echo "***********ENS***********"
    
     def response = httpRequest (
         httpMode: "POST",
