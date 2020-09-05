c25b447b3a39f43db85e0c53e377fbb753c5d630
M1053737 <54942737+M1053737@users.noreply.github.com>
2020-09-05 22:41:17 +0530

    Update createJenkinsBuildJob.groovy

--- vars/createJenkinsBuildJob.groovy
+++ vars/createJenkinsBuildJob.groovy
@@ -50,7 +50,7 @@ def call() {
     def response = httpRequest (
         httpMode: "POST",
         url: "http://52.172.43.67:8080/createItem?name=${jobName}",
-        customHeaders: [[name: 'Authorization', value: "Basic YWRtaW46YWRtaW4xMjM="], [name: 'Content-Type', value: 'application/xml'], [name: 'Jenkins-Crumb', value: "${crumbResponseMap.crumb}"],[name: 'Host', value: "52.172.43.67"]],
+        customHeaders: [[name: 'Authorization', value: "Basic YWRtaW46YWRtaW4xMjM="], [name: 'Content-Type', value: 'application/xml'], [name: 'Jenkins-Crumb', value: "${crumbResponseMap.crumb}"]],
         requestBody: "${payload}"
         //quiet: true
         //validResponseCodes: '200:403'
