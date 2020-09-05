fc8b8dce1c9e72797d64deaa4e40ea37525f8cea
M1053737 <54942737+M1053737@users.noreply.github.com>
2020-09-05 20:29:51 +0530

    Update createJenkinsBuildJob.groovy

--- vars/createJenkinsBuildJob.groovy
+++ vars/createJenkinsBuildJob.groovy
@@ -1,62 +1,34 @@
+
 import com.mulesoft.Constants
 import com.mulesoft.PipelinePlaceholders
-import com.mulesoft.Secrets
-import groovy.json.JsonSlurperClassic
-
-
 
 def call() {
     pipelinePlaceholders = PipelinePlaceholders.getInstance()
-    secrets = Secrets.getInstance()
     def jobName
 
     if(pipelinePlaceholders.getApiAssetId()) { // IMPLIES THIS IS AN API PROJECT
-        jobName = "(${pipelinePlaceholders.getDeploymentType()})-${pipelinePlaceholders.getApiAssetIdFormatted()}-(build)"  
-    }
-    else { // IMPLIES THIS IS A DOMAIN PROJECT
-        jobName = "(${pipelinePlaceholders.getDeploymentType()})-${pipelinePlaceholders.getDomainNameFormatted()}-(build)"
+        jobName = "(${pipelinePlaceholders.getDeploymentType()})-${pipelinePlaceholders.getApiAssetIdFormatted()}-(deploy)"
     }
 
-    def authString = "root:${secrets.getSecret('jenkins-api-token')}".getBytes().encodeBase64().toString()
-    //def authString = "root:${secrets.getSecret('jenkins-api-token')}"
-    echo authString
-   
-    
-    //def folderName=pipelinePlaceholders.getEnvironment()
-    echo "Jenkins"
-    //echo folderName
-    echo jobName
-    echo Constants.JENKINS_DOMAIN
+    def authString = "jenkins:${pipelinePlaceholders.getVaultSecret('jenkins-api-token')}".getBytes().encodeBase64().toString()
 
-    
-    
-    echo "******************KRISHNA START *************"
-     
-     def crumbResponse = httpRequest 'http://52.172.43.67:8080/crumbIssuer/api/json'
-         println("Status: "+crumbResponse.status)
-         println("Content: "+crumbResponse.content)
-     //def RawRecordsResponse = crumbResponse.content
-     //def json = new JsonSlurper().parseText(RawRecordsResponse)
-     //def crumbCode = json.'crumb'
-       // println("crumb Code: "+ crumbCode)
-        
+    def crumbResponse = httpRequest (
+        httpMode: "GET",
+        url: "https://${Constants.JENKINS_DOMAIN}/crumbIssuer/api/json",
+        customHeaders: [[name: 'Authorization', value: "Basic ${authString}"]],
+        quiet: true
+    )   
     def crumbResponseMap = new groovy.json.JsonSlurperClassic().parseText(crumbResponse.content)
-      println("crumb Code MAP : "+ crumbResponseMap)
-    echo "****************KRISHNA END**************"
-    echo crumbResponseMap.crumb
-    
-    def payload = readFile "buildConfig.xml"
-    echo payload
-   
+
+    def payload = readFile "deployConfig.xml"
+
     def response = httpRequest (
         httpMode: "POST",
-        url: "http://52.172.43.67:8080/createItem?name=${jobName}",
-        
-        customHeaders: [[name: 'Authorization', value: "Basic YWRtaW46YWRtaW4xMjM="], [name: 'Content-Type', value: 'application/xml'], [name: 'Jenkins-Crumb', value: "${crumbResponseMap.crumb}"]],
-        requestBody: "${payload}"
-        //quiet: true
-        //validResponseCodes: '200:403'
-     )
-    
-    sh "rm buildConfig.xml"
+        url: "https://${Constants.JENKINS_DOMAIN}/createItem?name=${jobName}",
+        requestBody: payload,
+        customHeaders: [[name: 'Authorization', value: "Basic ${authString}"], [name: 'Content-Type', value: 'application/xml'], [name: crumbResponseMap.crumbRequestField, value: crumbResponseMap.crumb]],
+        quiet: true
+    )
+
+    sh "rm deployConfig.xml"
 }
