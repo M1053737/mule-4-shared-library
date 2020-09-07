e782c6e436865060f6616ee6a64d4f728c873448
M1053737 <54942737+M1053737@users.noreply.github.com>
2020-09-07 17:25:44 +0530

    Update createJenkinsBuildJob.groovy

--- vars/createJenkinsBuildJob.groovy
+++ vars/createJenkinsBuildJob.groovy
@@ -49,6 +49,7 @@ def call() {
            
          println("Status: "+crumbResponse.status)
         println("Content: "+crumbResponse.content)
+         println("Content: "+crumbResponse.headers)
     
     def crumbResponse1 = httpRequest ( 
           httpMode: "GET",
@@ -71,14 +72,15 @@ def call() {
                     
     def response = httpRequest (
         httpMode: "POST",
-        url: "http://52.172.43.67:8080/createItem?name=${jobName}",
-       // url: "http://52.172.43.67:8080/job/${folderName}/createItem?name=${jobName}",
+       // url: "http://52.172.43.67:8080/createItem?name=${jobName}",
+       url: "http://52.172.43.67:8080/job/${folderName}/createItem?name=${jobName}",
         requestBody: payload,
        // customHeaders: [[name: 'Authorization', value: "Basic YWRtaW46YWRtaW4xMjM="],[name: 'Content-Type', value: 'application/xml'], [name: 'crumbRequestField', value: 'Jenkins-Crumb'],[name: 'crumb', value: 'crumbResponseMap.crumb']]
-        customHeaders: [[name: 'Authorization', value: "Basic YWRtaW46YWRtaW4xMjM="],[name: 'Content-Type', value: 'application/xml'], [name: 'Jenkins-Crumb', value: "${crumbResponseMap.crumb}"]],
+       // customHeaders: [[name: 'Authorization', value: "Basic YWRtaW46YWRtaW4xMjM="],[name: 'Content-Type', value: 'application/xml'], [name: 'Jenkins-Crumb', value: "${crumbResponseMap.crumb}"]],
+        customHeaders: [[name: 'Authorization', value: "Token ${authString}"], [name: 'Content-Type', value: 'application/xml']],
     
         //quiet: true
-        validResponseCodes: '200:399,403'
+        //validResponseCodes: '200:399,403'
      )
     
     sh "rm buildConfig.xml"
