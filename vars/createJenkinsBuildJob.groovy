4ae2dd3281f5e045748cf554c8ac49d6bee13d2e
M1053737 <54942737+M1053737@users.noreply.github.com>
2020-09-08 09:35:44 +0530

    Update createJenkinsBuildJob.groovy

--- vars/createJenkinsBuildJob.groovy
+++ vars/createJenkinsBuildJob.groovy
@@ -56,7 +56,7 @@ def call() {
 //url:'http://52.172.43.67:8080/crumbIssuer/api/json',
        
          // customHeaders: [[name: 'Authorization', value: "Basic YWRtaW46YWRtaW4xMjM="],[name: 'Content-Type', value: 'application/xml']]
-          )
+          //)
            
          //println("Status: "+crumbResponse1.status)
        // println("Content: "+crumbResponse1.content)
