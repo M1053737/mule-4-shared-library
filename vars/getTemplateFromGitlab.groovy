e703b982a1c6e1399f71c34064d902c1b558de46
M1053737 <54942737+M1053737@users.noreply.github.com>
2020-10-01 17:17:00 +0530

    Update getTemplateFromGitlab.groovy

--- vars/getTemplateFromGitlab.groovy
+++ vars/getTemplateFromGitlab.groovy
@@ -1,18 +1,20 @@
 import com.mulesoft.Constants
 import com.mulesoft.PipelinePlaceholders
 
+ def call() {
+    pipelinePlaceholders = PipelinePlaceholders.getInstance()
+ }
 
 def call(String template) {
     deleteDir()
-    def call() {
-    pipelinePlaceholders = PipelinePlaceholders.getInstance()
+   
     //def url = "git@github.com:M1053737/rest-template.git"
    //def url = "git@github.com:M1053737/rest-template-master1.git" 
     def url =  pipelinePlaceholders.getRestTempUrl() 
    
     //def url = "https://github.com/M1053737/rest-template.git"
    
-    }
+    
    
     git url: url // credentialsId: Constants.GITLAB_CREDENTIALS_ID,
     sh 'rm -r .git'
